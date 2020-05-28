8e2e659c7704c3ae3653d7486c69276f12f88fc7
Matheus Rocha <monteiromatheus.rocha@gmail.com>
2020-05-28 18:11:51 -0300

    quarintineeeee

---
+++ EXERCÍCIOS/recompensa.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+#define endl "\n"
+
+using namespace std;
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+   
+
+    
+    return 0;
+}
